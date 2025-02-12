e9648942a0f283223bfb482b59327ed0862aadb7
AlbertGimenezMasclans <150661196+AlbertGimenezMasclans@users.noreply.github.com>
2025-02-12 15:21:08 +0100

    Pagina registre iniciada

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase/paginas/pagina_registro.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner : false,
+      home: Scaffold(
+        body: PaginaRegistro()
+      ),
+    );
+  }
+}
