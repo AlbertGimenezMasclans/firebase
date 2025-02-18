c52ca9fbc9cea32a0afeb40ec9b4ea477b96d5b8
AlbertGimenezMasclans <150661196+AlbertGimenezMasclans@users.noreply.github.com>
2025-02-18 16:26:08 +0100

    Firebase configurar

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,17 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+
+/*
+  1. Tenir Node.js instalat.
+  2. Anar a la web de firebase i anar a la consola ('Go to console')
+    - Tenir el compte del roca iniciat
+  3. De de la consola, creem un projecte de Firebase
+  4. Anem al menu de compilacióni habilitem: 
+    - Authentitication i Firestore Database
+  5. En una cmd, per exemplela del Visual Studio Code, fem:
+    - npm install -g firebase-tools
+      (aixo instala Firebase al dispoitiu)
+  6. flutter pub global activate flutterfire_cli + dart pub global activate flutterfire_cli
+*/
